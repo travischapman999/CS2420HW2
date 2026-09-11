84757ee88bb7cb80681c4d4806c3110a5e582db0
travis.chapman <travis.chapman@ENGRGJCP5S3>
2026-09-11 10:24:17 -0600

    Add project files.

---
+++ CS2420HW2/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+
+
+public class LinkedList
+{
+    private Node head;
+    private Node tail;
+    private int length;
+    public LinkedList(int value)
+    {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+    public Node GetHead() { return head; }
+    public Node GetTail() { return tail; }
+    public int GetLength() { return length; }
+    public void RemoveDuplicates()
+    {
+        Node current = head;
+        while(current.next.value != void)
+            {
+            if (current.value == current.next.value)
+            {
+                current.next = current.next.next;
+            }
+        }
+    }
+}
+public class Node
+{
+    public int value;
+    public Node next;
+    public Node prev;
+    public Node(int value)
+    {
+        this.value = value;
+    }
+}
